3de5eb5f8418cd1185995a2772fb1e71f2f9e30d
nirutk <nirutk@vgi.co.th>
2025-04-10 20:22:42 +0700

    init

---
+++ user-activity-producer/src/main/java/com/activity/user_activity_producer/UserActivityProducerApplication.java
@@ -0,0 +1,13 @@
+package com.activity.user_activity_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserActivityProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserActivityProducerApplication.class, args);
+	}
+
+}
